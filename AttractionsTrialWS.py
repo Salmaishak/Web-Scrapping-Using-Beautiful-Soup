3cc324d957547212b2fbc06ad278861a4cc96cdc
Salma Magdy Ishak <salma.m.ishakk@gmail.com>
2022-11-09 19:20:04 +0200

    attractions after arabic name edit

--- AttractionsTrialWS.py
+++ AttractionsTrialWS.py
@@ -5,23 +5,26 @@ import pandas as pd
 import csv
 
 # Extract the HTML and create a BeautifulSoup object.
-url = ('https://www.tripadvisor.in/Attractions-g294201-Activities-oa0-Cairo_Cairo_Governorate.html')
+from googletrans import Translator
 
-user_agent = ({'User-Agent':
-			'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0',
-			'Accept-Language': 'en-US, en;q=0.5'})
+url = 'https://www.tripadvisor.in/Attractions-g294201-Activities-oa0-Cairo_Cairo_Governorate.html'
 
-def get_page_contents(url):
-    page = requests.get(url, headers = user_agent)
-    return BeautifulSoup(page.text, 'html.parser')
 
-soup = get_page_contents(url)
+user_agent = ({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0',
+               'Accept-Language': 'en-US, en;q=0.5'})
+
+page = requests.get(url, headers=user_agent)
+soup = BeautifulSoup(page.text, 'html.parser')
 
 # Find and extract the data elements.
 attractions = []
 reviews=[]
 for name in soup.findAll('div',{'class':'XfVdV o AIbhI'}):
-   attractions.append(name.text)
+    translator = Translator()
+    translation = translator.translate(name.text, dest='ar')
+    attractions.append(translation.text)
+
+
 for review in soup.findAll('span',{'class':'biGQs _P pZUbB biKBZ osNWb'}):
     reviews.append(review.text.strip())
 # Create the dictionary.
@@ -32,4 +35,4 @@ cairo = pd.DataFrame.from_dict(dict)
 cairo.head(10)
 
 # Convert dataframe to CSV file.
-cairo.to_csv('attractions.csv', index=False, header=True)
+cairo.to_csv('attractions.csv', index=False, header=True,encoding='utf-8-sig')
