334275baeae3a8e399007d3ffd6e243aef150d57
Salah Ashraf <salahashraf924@gmail.com>
2022-12-01 00:56:05 +0200

    add function display data

--- RestaurantsPackage/Restaurant.py
+++ RestaurantsPackage/Restaurant.py
@@ -12,3 +12,17 @@ class Restaurant:
     cuisines=""
     special_diets=""
 
+    def displayData(self):
+        print("Id: "+self.id)
+        print("Name: "+ self.name)
+        print("City: "+self.city)
+        print("location: "+self.location)
+        print("phone_no: "+self.phone_no)
+        print("website link: "+self.website_link)
+        print("rate: "+self.rate)
+        print("price range: "+self.price_range)
+        print("cuisines: "+self.cuisines)
+        print("special diets: "+self.special_diets)
+        print("Reviews: ")
+        for review in self.reviews:
+            print(review)
