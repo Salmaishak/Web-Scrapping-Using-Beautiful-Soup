ef3dc033350173c376a6dc5b0a6a3dd284a49dd0
Salah Ashraf <salahashraf924@gmail.com>
2022-12-05 02:04:02 +0200

    fix append issue

--- RestaurantsPackage/CSV_Writer.py
+++ RestaurantsPackage/CSV_Writer.py
@@ -1,24 +1,33 @@
 import csv
-headers=["Name","City","phone_no","location","price_range","cuisines","special_diets","open_time","close_time","rate","number of reviews"]
 
-def add_restaurant(resturants_list):
-    with open('restaurants_data.csv', 'w',encoding='utf-8',errors='ignore',newline='') as file:
+headers = ["Name", "City", "phone_no", "location", "price_range", "cuisines", "special_diets", "open_time",
+           "close_time", "rate", "number of reviews"]
+
+initial_write=True
+
+def add_restaurant(restaurant, initial_write):
+    with open('restaurants_data_alex.csv', mode='a', encoding='utf-8', errors='ignore', newline='') as file:
         writer = csv.writer(file)
         # r.displayData()
-        writer.writerow(headers)
-        for r in resturants_list:
-            data = [r.name, r.city, r.phone_no, r.location, r.price_range, r.cuisines, r.special_diets, r.open_time,
-                    r.close_time, r.rate,r.number_of_reviews]
-            writer.writerow(data)
-    write_reviews(resturants_list)
+        if initial_write is True:
+            writer.writerow(headers)
+            write_reviews(restaurant, initial_write)
+            return
+        data = [restaurant.name, restaurant.city, restaurant.phone_no, restaurant.location, restaurant.price_range, restaurant.cuisines, restaurant.special_diets, restaurant.open_time,
+                restaurant.close_time, restaurant.rate, restaurant.number_of_reviews]
+        writer.writerow(data)
 
-def write_reviews(resturants_list):
-    with open('restaurant_review.csv', 'w', encoding='utf-8', errors='ignore',newline='') as file:
+    write_reviews(restaurant,initial_write)
+
+
+def write_reviews(restaurant,initial_write):
+    with open('alex_reviews.csv', mode='a', encoding='utf-8', errors='ignore', newline='') as file:
         writer = csv.writer(file)
-        headers=["restaurant_name","review"]
-        writer.writerow(headers)
-        for restaurant in resturants_list:
-            for review in restaurant.reviews:
-                data= [restaurant.name, review]
-                writer.writerow(data)
+        if initial_write is True:
+            headers = ["restaurant_name", "review"]
+            writer.writerow(headers)
+            return
+        for review in restaurant.reviews:
+            data = [restaurant.name, review]
+            writer.writerow(data)
 
