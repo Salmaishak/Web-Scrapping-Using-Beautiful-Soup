5d05a4bf10d6abe779872e5194eed1600a91b023
Salah Ashraf <salahashraf924@gmail.com>
2022-12-09 14:46:52 +0200

    remove get reviews function

--- RestaurantsPackage/CSV_Writer.py
+++ RestaurantsPackage/CSV_Writer.py
@@ -6,12 +6,11 @@ headers = ["Name", "City", "phone_no", "location", "price_range", "cuisines", "s
 initial_write=True
 
 def add_restaurant(restaurant, initial_write):
-    with open('restaurants_data_alex2.csv', mode='a', encoding='utf-8', errors='ignore', newline='') as file:
+    with open('Luxor.csv', mode='a', encoding='utf-8', errors='ignore', newline='') as file:
         writer = csv.writer(file)
         # r.displayData()
         if initial_write is True:
             writer.writerow(headers)
-            write_reviews(restaurant, initial_write)
             return
         data = [restaurant.name, restaurant.city, restaurant.phone_no, restaurant.location, restaurant.price_range,
                 restaurant.cuisines, restaurant.special_diets,
@@ -19,17 +18,5 @@ def add_restaurant(restaurant, initial_write):
                 restaurant.close_time, restaurant.rate, restaurant.number_of_reviews]
         writer.writerow(data)
 
-    write_reviews(restaurant,initial_write)
 
-
-def write_reviews(restaurant,initial_write):
-    with open('alex_reviews2.csv', mode='a', encoding='utf-8', errors='ignore', newline='') as file:
-        writer = csv.writer(file)
-        if initial_write is True:
-            headers = ["restaurant_name", "review"]
-            writer.writerow(headers)
-            return
-        for review in restaurant.reviews:
-            data = [restaurant.name, review]
-            writer.writerow(data)
 
