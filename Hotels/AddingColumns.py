c78e8039d18c6ab44e17c62a49efd7addfef7608
RawanGamalElDin12 <rawan.gamaleldin@outlook.com>
2022-12-12 00:08:33 +0200

    Adding city column to cairo hotels

--- Hotels/AddingColumns.py
+++ Hotels/AddingColumns.py
@@ -5,14 +5,14 @@ giza = pd.read_csv('HotelsGiza.csv')
 luxor = pd.read_csv('HotelsLuxor.csv')
 hurghada = pd.read_csv('HotelsHurgada2.csv')
 sharm = pd.read_csv('HotelsSharm.csv')
-
+cairo = pd.read_csv('Hotels.csv')
 fayoum["City"] = "Al-Fayyum"
 alex["City"] = "Alexandria"
 giza["City"] = "Giza"
 sharm["City"] = "Sharm El Sheikh"
 hurghada["City"] = "Hurghada"
 luxor["City"] = "Luxor"
-
+cairo["City"] = "Cairo"
 #fayoum.to_csv('Hotelsfayoum.csv',index=False, encoding="utf-8", mode='w')
 #alex.to_csv('HotelsAlexandria.csv',index=False, encoding="utf-8", mode='w')
 #luxor.to_csv('HotelsLuxor.csv',index=False, encoding="utf-8", mode='w')
@@ -20,4 +20,4 @@ luxor["City"] = "Luxor"
 #sharm.to_csv('HotelsSharm.csv', index=False, encoding="utf-8", mode='w')
 #giza.to_csv('HotelsGiza.csv', index=False, encoding="utf-8", mode='w')
 
-
+#cairo.to_csv('Hotels.csv', index=False, encoding="utf-8", mode='w')
