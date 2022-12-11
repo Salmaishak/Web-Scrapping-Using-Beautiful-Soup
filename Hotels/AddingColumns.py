c9eb9e8ac4f07969865c3a776e17992886229e43
RawanGamalElDin12 <rawan.gamaleldin@outlook.com>
2022-12-12 00:05:25 +0200

    Addling City Column to All CSV files

---
+++ Hotels/AddingColumns.py
@@ -0,0 +1,23 @@
+import pandas as pd
+fayoum = pd.read_csv('HotelsFayoum.csv')
+alex = pd.read_csv('HotelsAlexandria.csv')
+giza = pd.read_csv('HotelsGiza.csv')
+luxor = pd.read_csv('HotelsLuxor.csv')
+hurghada = pd.read_csv('HotelsHurgada2.csv')
+sharm = pd.read_csv('HotelsSharm.csv')
+
+fayoum["City"] = "Al-Fayyum"
+alex["City"] = "Alexandria"
+giza["City"] = "Giza"
+sharm["City"] = "Sharm El Sheikh"
+hurghada["City"] = "Hurghada"
+luxor["City"] = "Luxor"
+
+#fayoum.to_csv('Hotelsfayoum.csv',index=False, encoding="utf-8", mode='w')
+#alex.to_csv('HotelsAlexandria.csv',index=False, encoding="utf-8", mode='w')
+#luxor.to_csv('HotelsLuxor.csv',index=False, encoding="utf-8", mode='w')
+#hurghada.to_csv('HotelsHurgada2.csv', index=False, encoding="utf-8", mode='w')
+#sharm.to_csv('HotelsSharm.csv', index=False, encoding="utf-8", mode='w')
+#giza.to_csv('HotelsGiza.csv', index=False, encoding="utf-8", mode='w')
+
+
